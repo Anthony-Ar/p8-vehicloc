d03d47e827fb9eadf6be63d88bd83d65681b5dcc
Anthony <anthony.arrighi@orange.fr>
2025-04-05 08:31:13 +0200

    Ajout de commentaires sur les fonctions

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,6 +9,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
+    /**
+     * Affiche la page d'accueil de l'application
+     * @param CarRepository $carRepository
+     * @return Response
+     */
     #[Route('/', name: 'app_home')]
     public function index(CarRepository $carRepository): Response
     {
