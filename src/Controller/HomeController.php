ca9601df4d93cdf0e6733b813cc15faf41ba8523
Anthony <anthony.arrighi@orange.fr>
2025-04-28 09:41:25 +0200

    refactor: Main > Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class MainController extends AbstractController
+final class HomeController extends AbstractController
 {
     /**
      * Affiche la page d'accueil de l'application
